97e67ef5c79cb072f66e8bba9b0bbf570342e532
Kamila Juszczyk <Kamila.Juszczyk86@onet.pl>
2024-10-15 16:48:17 +0200

    zadanie 0.8

---
+++ kodilla-intro/src/main/java/com/kodilla/Zadanie2.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        String myName = "Dawid";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+    }
+}
