0631895b9387b61b54df38ef34845ff68e3e4a9e
Kamila Juszczyk <Kamila.Juszczyk86@onet.pl>
2024-10-17 17:04:24 +0200

    Nowe

--- kodilla-intro/src/main/java/com/kodilla/MainExtended.java
+++ kodilla-intro/src/main/java/com/kodilla/MainExtended.java
@@ -4,35 +4,21 @@ import javax.print.DocFlavor;
 
 public class MainExtended {
     public static void main(String[] args) {
-        int wiek=100;
+
         boolean amiold=false;
         double weight=50;
         char znak='A';
         String String = "Witaj";
         float BMI=15493;
         long bity=34567;
-        System.out.println(bity);
+
+System.out.println(znak);
+
+        }
 
 
-        System.out.println(BMI);
-
-
-
-        System.out.println(weight);
-        System.out.println(znak);
-        System.out.println(String);
-
-
-        System.out.println(amiold);
-
-
-        System.out.println(wiek);
-        System.out.println(wiek);
-        System.out.println(wiek);
-        System.out.println(wiek);
-System.out.println(wiek);
-        System.out.println(wiek);
-        System.out.println(wiek);
-        System.out.println(wiek);
     }
-}
+
+
+
+
