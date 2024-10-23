1fc72a28f2c14ff4cf3cdf62613b41c8dbef02b6
Kamila Juszczyk <Kamila.Juszczyk86@onet.pl>
2024-10-23 17:04:32 +0200

    praca domowa

---
+++ kodilla-intro/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String powitanie = "Hello World";
+        int wiek = 22;
+        boolean=tru;
+        char ='G';
+    }
+        System.out.println(powitanie);
+
+
+
+    }
+
+
+
+}
