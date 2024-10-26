84e31d21c9ce5ac5b2d4061159fd7f78b4196425
Kamila Juszczyk <Kamila.Juszczyk86@onet.pl>
2024-10-26 14:18:31 +0200

    praca1

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,10 +1,10 @@
 public class FirstClass {
-    public static String good() {
-        return "To jest dobry przykład!";
+    public static String dance() {
+        return "To jest dobry taniec!";
     }
 
     public static void main(String[] args) {         // [2]
-        String example = good();
+        String example = dance();
 
         System.out.println(example);// [3]
     }
