996a1594d7a5d7b6c52c08abd78738dd3d01462f
Kamila Juszczyk <Kamila.Juszczyk86@onet.pl>
2024-11-13 18:10:43 +0100

    tablica1

--- kodilla-intro/SimpleArrary.java
+++ kodilla-intro/SimpleArrary.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class SimpleArrary {
+    public static void main(String[] args) {
+        String []animal = new String[6];
+ animal[0] = "cat";
+ animal[1] = "dog";
+ animal[2] = "mouse";
+ animal[3] = "panda";
+ animal[4] = "cow";
+ animal[5] = "rabbit";
+
+    }
+
+    String animal = animal[2];
+}
+
+
+
 }
--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
+        czyRokJestPrzestepny(2025);
         int firstNumber = 11;
         int secondNumber = 22;
 
@@ -23,6 +24,32 @@ public class FirstClass {
 
         System.out.println(result);
     }
+    private static void czyRokJestPrzestepny(int rok){
+        if(rok%4==0){
+            if(rok%100==0){
+                if(rok%400==0){
+                    System.out.println(rok+"przestepny");
+
+
+                }
+                else{
+                    System.out.println(rok+"nie przestepny");
+                }
+
+
+            }
+            else{
+                System.out.println(rok+"przestepny");
+            }
+        }
+        else {
+            System.out.println(rok + "nie jest przestepny");
+        }
+    }
+    String[] movies = new String[3];
+    movies[0] = "Harry Potter";
+    movies[1] = "Lord of the Rings";
+    movies[2] = "Forest Gump";
 }
 
 
